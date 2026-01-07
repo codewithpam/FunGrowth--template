6001d5c6b471fd57222561ddc4fd102996363a79
comewithpam <padmny@gmail.com>
2026-01-07 11:34:26 +0100

    Commit to force redeploy

--- src/components/ActivityForm.tsx
+++ src/components/ActivityForm.tsx
@@ -20,7 +20,7 @@ const ActivityForm = () => {
 
   try {
     setLoading(true);
-    //use HTTPS API endpoint for PDF generation
+    //use HTTPS API endpoint for PDF  generation
     const API_URL = import.meta.env.VITE_API_URL;
     const response = await fetch(`${API_URL}/generate-pdf`, {
       method: "POST",
